e194ae19678b74c53f1cc2adea5bf8d98f9145d0
JoshFourie <josh@omniaprotocol.com>
2019-01-15 11:43:37 +1100

    interface: impl gen_ed25519_key_pairing associated f(x) on Amelia struct, moved test to interface layer, lib: added pub fn for getting ed25519 key_pairings,

--- src/interface.rs
+++ src/interface.rs
@@ -1,30 +1,62 @@
 use zero_orb::{
-    Andromeda, InterOperable, Transportable,
+    Andromeda, GoZero, 
     Knowledge, 
+    crypto::{EdDSA, SignatureScheme},
     CommonReference, Common,
     FrLocal, G1Local, G2Local,
 };
 use serde_derive::{Serialize, Deserialize};
-use serde_json::from_str;
 
 #[derive(Serialize, Deserialize)]
 pub struct Amelia {
-    pub crs: String,
-    pub sleep: usize,
-    pub calories: usize,
+    crs: String,
+    sleep: usize,
+    calories: usize,
+    output: usize,
+    key_pair: String,
 }
 
 impl Amelia {
-    pub fn go_andromeda(s: String) -> String {
-        let amelia: Amelia = from_str(&s).expect("Amelia::go_andromeda() deserializing Amelia to struct");
-        let k = Knowledge::init(
+    pub fn go_andromeda(json_string: String) -> String {
+        let amelia: Amelia = serde_json::from_str(&json_string)
+            .expect("Amelia::go_andromeda() deserializing Amelia to struct");
+        let proof_struct = Knowledge::into(
             None,
             None,
             Some(vec![amelia.sleep, amelia.calories]),
             None,
             None,
         );
-        let crs = CommonReference::<FrLocal, G1Local, G2Local>::read(&amelia.crs);
-        Andromeda::init(crs, k).go().wrap_as_str()        
+        let key_pair: Box<[u8]> = serde_json::from_str(&amelia.key_pair)
+            .expect("Amelia::go_andromeda() deserializing key_pair to Box<[u8>"); 
+        let common_reference_string = CommonReference::<FrLocal, G1Local, G2Local>::read(&amelia.crs);
+        serde_json::to_string(
+            &Andromeda::into(
+                common_reference_string,
+                proof_struct,
+                Some(vec![amelia.output]),
+                key_pair,
+            ).go()
+        ).expect("Amelia::go_andromeda()::to_string() panicked")
+    } 
+    pub fn gen_ed25519_key_pairing() -> String {
+        serde_json::to_string(
+            &EdDSA::<String>::init_key_pair()
+        ).expect("Amelia::gen_ed25519_key_pairing()::to_string() serializing EdDSA::KeyPair")
     }
+}
+
+#[test]
+fn test_go_andromeda() {
+    let amelia_serialized = serde_json::to_string(
+        &Amelia {
+            crs: std::fs::read_to_string("src/tests/sample.crs")
+                .expect("internal_tests: reading sample.crs to String"),
+            sleep: 4,
+            calories: 2,
+            output: 8,
+            key_pair: Amelia::gen_ed25519_key_pairing(),
+        }
+    ).expect("internal_tests: Serializing &x to String");
+    println!("{}", Amelia::go_andromeda(amelia_serialized));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,9 @@
 pub mod interface;
-use crate::interface::go_andromeda;
 
 pub fn parse_amelia(s: String) -> String {
-    go_andromeda(s)
+    interface::Amelia::go_andromeda(s)
 }
 
-#[test]
-fn test_amelia() {
-    use crate::interface::Amelia;
-    use std::fs::read_to_string;
-    use serde_json::to_string;
-
-    let x = Amelia {
-        crs: read_to_string("src/tests/sample.crs").expect("internal_tests: reading sample.crs to String"),
-        sleep: 4,
-        calories: 2400,
-    };
-    let s = to_string(&x).expect("internal_tests: Serializing &x to String");
-    parse_amelia(s);
+pub fn gen_amelia_ed25519_key_pairing() -> String {
+    interface::Amelia::gen_ed25519_key_pairing()
 }
